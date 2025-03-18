b8c23ddb61a763408d423c9358ca49aa45718c6d
Derrick Katute <derrick.katute@student.moringaschool.com>
2025-03-18 11:15:09 +0300

    add images of ramen in the constant tab

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "images/shoyu.jpg", rating: 5, comment: "Delicious!" },
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "images/kojiro.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "images/tonkotsu.jpg", rating: 3, comment: "Rich broth!" }
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "images/tonkotsu.jpg", rating: 3, comment: "Rich broth!" },
+    { id: 4, name: "Naruto Ramen", restaurant: "Naruto-ya", image: "images/naruto.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 5, name: "Nirvana Ramen", restaurant: "Nirvana-ya", image: "images/nirvana.jpg", rating: 5, comment: "Delicious!" },
 ];
 
 // Function to display all ramen images in the menu
