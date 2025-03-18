09964ef8a479d83b134f6710362295115b092ab2
Derrick Katute <derrick.katute@student.moringaschool.com>
2025-03-18 09:57:09 +0300

    changes in script

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Sample ramen data
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg", rating: 3, comment: "Rich broth!" }
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "images/kojiro.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "images/tonkotsu.jpg", rating: 3, comment: "Rich broth!" }
 ];
 
 // Function to display all ramen images in the menu
